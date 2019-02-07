dfe48c03ab25080c15d58b39bc36d1003eb5b536
Greg Kittleson <greg.kittleson@gmail.com>
2019-02-07 14:58:44 -0800

    Refactor restaurant_id foreign key in reservations

--- database/schema.sql
+++ database/schema.sql
@@ -1,15 +1,8 @@
-CREATE DATABASE reservations;
+-- CREATE DATABASE reservations;
 USE reservations;
 
-CREATE TABLE reservations {
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  restaurant_id FOREIGN KEY,
-  date DATE,
-  time TIME,
-  party_size INT
-}
 
-CREATE TABLE restaurants {
+CREATE TABLE restaurants (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   max_party_size INT,
   max_days_to_book INT,
@@ -18,4 +11,13 @@ CREATE TABLE restaurants {
   start_hour TIME,
   end_hour TIME,
   bookings_today INT
-}
+);
+
+CREATE TABLE reservations (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  restaurant_id INT,
+  date DATE,
+  time TIME,
+  party_size INT,
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants(id)
+);
