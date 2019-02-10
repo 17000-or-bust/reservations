2243e79a93145354e01effeee8802529464ec468
Greg Kittleson <greg.kittleson@gmail.com>
2019-02-10 12:19:23 -0800

    Clean up code

--- spec/components/Booked.test.js
+++ spec/components/Booked.test.js
@@ -3,12 +3,6 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Booked from '../../client/components/Booked.jsx';
 
-// import babelpolyfill
-// const React = require('react');
-// const Enzyme = require('enzyme');
-// const Adapter = require('enzyme-adapter-react-16');
-// const Booked = require('../../client/components/Booked.jsx');
-
 configure({ adapter: new Adapter() });
 
 describe('Booked component', () => {
