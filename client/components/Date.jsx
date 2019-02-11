2f31082577580e688e127ff0bdf9ba83cd52963d
Greg Kittleson <greg.kittleson@gmail.com>
2019-02-10 22:42:36 -0800

    Reformat calendar divs into grid pattern

--- client/components/Date.jsx
+++ client/components/Date.jsx
@@ -2,26 +2,48 @@ import React from 'react';
 import moment from 'moment';
 
 let Date = () => {
-  var days = [];
+  var dates = [];
+  var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   var firstWeek = moment()
     .startOf('month')
     .week();
   var lastWeek = firstWeek + 5;
 
   for (var i = firstWeek; i <= lastWeek; i++) {
+    var oneWeek = [];
     for (var j = 0; j <= 6; j++) {
       var day = moment()
         .week(i)
         .startOf('week')
         .add(j, 'day');
-      days.push(day);
+      oneWeek.push(day);
     }
+    dates.push(oneWeek);
   }
 
   return (
     <div id="date">
-      {days.map(day => {
-        return <div>{day.format('D')}</div>;
+      <div id="days">
+        {days.map(day => {
+          return (
+            <div className="day" key={day}>
+              {day}
+            </div>
+          );
+        })}
+      </div>
+      {dates.map(week => {
+        return (
+          <div className="row" key={week}>
+            {week.map(day => {
+              return (
+                <div className="box" key={day}>
+                  {day.format('D')}
+                </div>
+              );
+            })}
+          </div>
+        );
       })}
     </div>
   );
