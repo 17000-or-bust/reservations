11d53c8f9335a99be94d55c41bf662b727ef07a8
ecuyle <ecuyle@gmail.com>
2019-02-28 23:21:27 -0800

    Implement DataGenerator class
    DataGenerator class is the new implementation used to generate
    production-grade data sets. The .generate method, when using
    a CHUNK_SIZE of 20,000 and MAX_ROWS count of 10,000,000, can generate
    two .csv files of 10,000,000 lines each in roughly 49 seconds whilst
    only consuming roughly 60mb of RAM in the process. This was achieved
    by implementing both a Readable and Writable stream to reduce the load
    on memory at any given point in time.
    
    This is a huge improvement from before as the solution using only
    Writable streams took roughly 90 seconds to run and consumed roughly
    1.5GB of RAM in the process.

--- generator/index.js
+++ generator/index.js
@@ -1,63 +1,55 @@
-const fs = require('fs');
-const faker = require('faker');
 const moment = require('moment');
-const startTime = Date.now();
-console.log(`Starting generator at ${moment().format('HH:MM:SS')}`);
+const DataGenerator = require('./DataGenerator.js');
+
+// Constants
+const CHUNK_SIZE = 20000;
+const MAX_ROWS = 10 * 1000 * 1000;
+
+// Helper functions
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 const getRandomHour = (min, max) => {
-  return `${faker.random.number({ min, max })}:00`;
+  return `${getRandomInt(min, max)}:00`;
 };
 
 const getRandomDateUpToNMonths = maxMonths => {
-  return moment(faker.date.between(moment().format('YYYY-MM-DD'), moment().add(maxMonths, 'months').format('YYYY-MM-DD'))).format('YYYY-MM-DD');
+  const date = new Date().getTime();
+  let futureDate = new Date();
+  const dateFromNow = futureDate.setMonth(futureDate.getMonth() + maxMonths);
+  return moment(getRandomInt(date, dateFromNow)).format('YYYY-MM-DD');
 };
 
-const seedRestaurants = () => {
-  const restWriteable = fs.createWriteStream('../database/restaurants.csv');
-  let buffer = '';
-  for (let j = 0; j < 1000; j++) {
-    for (var i = 0; i < 10000; i++) {
-      const id = (j * 10000) + i;
-      let maxPartySize = Math.round(Math.random() * 16 + 4);
-      let maxDaysToBook = Math.round(Math.random() * 83 + 7);
-      let hasRewards = Math.round(Math.random());
-      if (Math.round(Math.random())) {
-        var timeSlotInterval = '00:15:00';
-      } else {
-        var timeSlotInterval = '00:30:00';
-      }
-      let startHour = getRandomHour(0, 12);
-      let endHour = getRandomHour(13, 24);
-      let bookingsToday = Math.round(Math.random() * 150);
-
-      buffer += `${id},${maxPartySize},${maxDaysToBook},${hasRewards},"${timeSlotInterval}","${startHour}","${endHour}",${bookingsToday}\n`;
-    }
-    restWriteable.write(buffer);
-    buffer = '';
+const generateRestaurant = () => {
+  const maxPartySize = Math.round(Math.random() * 16 + 4);
+  const maxDaysToBook = Math.round(Math.random() * 83 + 7);
+  const hasRewards = Math.round(Math.random());
+  if (Math.round(Math.random())) {
+    var timeSlotInterval = '00:15:00';
+  } else {
+    var timeSlotInterval = '00:30:00';
   }
+  const startHour = getRandomHour(0, 12);
+  const endHour = getRandomHour(13, 24);
+  const bookingsToday = Math.round(Math.random() * 150);
+
+  return `${maxPartySize},${maxDaysToBook},${hasRewards},"${timeSlotInterval}","${startHour}","${endHour}",${bookingsToday}`;
 };
 
-const seedReservations = n => {
-  const resWriteable = fs.createWriteStream('../database/reservations.csv');
-  let buffer = '';
-  for (let j = 0; j < 1000; j++) {
-    for (var i = 0; i < 10000; i++) {
-      const id = (j * 10000) + i;
-      let restaurantId = faker.random.number(9999999);
-      let date = getRandomDateUpToNMonths(3);
-      let time = getRandomHour(0, 24);
+const generateReservation = () => {
+  const restaurantId = getRandomInt(0, 9999999);
+  const date = getRandomDateUpToNMonths(3);
+  const time = getRandomHour(0, 24);
 
-      buffer += `${id},${restaurantId},"${date}","${time}"\n`;
-    }
-    resWriteable.write(buffer);
-    buffer = '';
-  }
+  return `${restaurantId},"${date}","${time}"`;
 };
 
-console.log(`Generating restaurant data....`);
-seedRestaurants();
-console.log(`Generating reservations data....`);
-seedReservations();
-console.log(`Writing data to files....`);
-const endTime = Date.now();
-console.log(`Success. Elapsed time: ${moment(endTime).diff(moment(startTime), 'seconds')} seconds`);
+// Generate csv files
+const restaurantGenerator = new DataGenerator(generateRestaurant, CHUNK_SIZE, MAX_ROWS, '../database/restaurants.csv');
+const reservationGenerator = new DataGenerator(generateReservation, CHUNK_SIZE, MAX_ROWS, '../database/reservations.csv');
+
+restaurantGenerator.generate();
+reservationGenerator.generate();
